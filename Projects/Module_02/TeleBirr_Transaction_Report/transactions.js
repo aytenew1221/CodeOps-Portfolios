02e8932dc4fdcecb3b54a96a071b943568267614
aytenew <aytenew.ayele2@gmail.com>
2026-08-13 20:28:04 +0300

    Add transaction data

---
+++ Projects/Module_02/TeleBirr_Transaction_Report/transactions.js
@@ -0,0 +1,36 @@
+// transactions.js
+
+const transactions = [
+  {
+    id: 1,
+    customer: "Abebe",
+    amount: 500,
+    type: "credit",
+  },
+  {
+    id: 2,
+    customer: "Sara",
+    amount: 300,
+    type: "debit",
+  },
+  {
+    id: 3,
+    customer: "Dawit",
+    amount: 1000,
+    type: "credit",
+  },
+  {
+    id: 4,
+    customer: "Hana",
+    amount: 250,
+    type: "debit",
+  },
+  {
+    id: 5,
+    customer: "Mekdes",
+    amount: 750,
+    type: "credit",
+  },
+];
+
+export default transactions;
