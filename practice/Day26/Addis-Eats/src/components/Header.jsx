e384607bdfcb7049cf985979c10980a91bc0f19c
aytenew <aytenew.ayele2@gmail.com>
2026-08-29 15:02:21 +0300

    Create Header component

---
+++ practice/Day26/Addis-Eats/src/components/Header.jsx
@@ -0,0 +1,10 @@
+function Header() {
+  return (
+    <header className="header">
+      <h1>addis-eats</h1>
+      <p>this is addis eats menu for tradational ethiopian food list</p>
+    </header>
+  );
+}
+
+export default Header;
