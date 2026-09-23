3cb1ae196ad33e2d6c207f34edd798dc0fcb610a
aytenew <aytenew.ayele2@gmail.com>
2026-09-23 16:03:59 +0300

    feat: add menu page

---
+++ practice/addis-eats-next/src/app/menu/page.js
@@ -0,0 +1,13 @@
+import DishList from "./DishList";
+
+export default function MenuPage() {
+  return (
+    <section>
+      <h1>Our Menu</h1>
+
+      <p>Choose your favorite Ethiopian dishes.</p>
+
+      <DishList />
+    </section>
+  );
+}
